60d2391a3cd24d39990868a4c1ab6f5da6ed932a
ogr0818 <sammy336588@gmail.com>
2026-01-30 16:08:51 +0800

    add the tabel

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import streamlit as st
 from plotly import express as px
 import re
@@ -11,23 +12,6 @@ with open(r'./statics/style.css') as f:
 
 tab1, tab2 = st.tabs(["耗量輸入頁面", "📈 急救盤用藥分析"])
 with tab1:
-    st.title(":blue[急救盤用藥分析]")
-    file_path = "./EER.xlsx"    # Path(uploaded_file).name
-    
-    uploaded_file = st.file_uploader("需要時上傳檔案：", type="xlsx")
-    if uploaded_file is None:
-        data = pd.read_excel(file_path)
-        st.markdown(f'<div style="text-align: center;">\
-                    <h5 style="color:red";>目前檔案為內存樣本</h5>\
-                    </div>',
-                    unsafe_allow_html=True)
-    else:
-        desktop_path = Path.home() / "Desktop"
-        uploaded_desktop = st.file_uploader("📟檔案：", type="xlsx")
-        if uploaded_desktop is not None:
-            st.write(f"檔案大小為{uploaded_desktop.size}")
-        # st.write(f"{pd.read_excel(uploaded_file).shape[0]}")
-    st.divider()
     department = st.radio("護理站/部外/ER", ['護理站', '部外單位', 'ER'], index=0, horizontal=True)
     stations = ['10', '103', '11', '12', '15', '日間', '25/26', '新生兒加護中心', '31/32', '41', '42', '51', '52', '53', '61', '62', '63', '71', '73', '81', '82', '83', '91', '92', '93', 'CCU', 'ICU-G', 'ICU-M', 'ICU-S']
     dep = ['健康管理中心', '內視鏡(胃鏡)室', '影像醫學部', '復健部', '心導管室', '心臟超音波室', '心臟重建科', '心電圖室(EKG室)', '手術室', '放射治療科', '核子醫學科', '高壓氧中心', '洗腎室']
@@ -70,5 +54,31 @@ with tab1:
     vol_Vitagen50 = st.number_input('Vitagen(50%): &nbsp;', min_value=0, max_value=30, value=0)
     vol_Vitagen20 = st.number_input('Vitagen(20%): &nbsp;', min_value=0, max_value=30, value=0)
 
+    cols =['Adrenalin', 'Adenocor', 'Agglutex', 'Atropine', 'Cordarone', 'Diphenhydramine', 'Dopamin', 'Gendobu', 'Lanoxin', 'MgSO4',
+            'Norepinephrine', 'Rolikan', 'Isoproternol', 'Solu-cortef', 'U-Sodin', 'VitaCal', 'Vitagen(50%)', 'Vitagen(20%)']
+    
+    values = [vol_Adrenalin, vol_Adenocor, vol_Agglutex, vol_Atropine, vol_Cordarone, vol_Diphenhydramine, vol_Dopamin,
+              vol_Gendobu, vol_Lanoxin, vol_MgSO4, vol_Norepinephrine, vol_Rolikan, vol_Isoproternol, vol_Solucortef,
+              vol_USodin, vol_VitaCal, vol_Vitagen50, vol_Vitagen20]
+    st.divider()
+    reshape = list(zip(cols,values))
+    record = pd.DataFrame(reshape, columns=['藥名', '耗用量'])
+    st.write(record)
+
 with tab2:
     st.title(":blue[急救盤用藥分析]")
+    file_path = "./EER.xlsx"    # Path(uploaded_file).name
+    
+    uploaded_file = st.file_uploader("需要時上傳檔案：", type="xlsx")
+    if uploaded_file is None:
+        data = pd.read_excel(file_path)
+        st.markdown(f'<div style="text-align: center;">\
+                    <h5 style="color:red";>目前檔案為內存樣本</h5>\
+                    </div>',
+                    unsafe_allow_html=True)
+    else:
+        desktop_path = Path.home() / "Desktop"
+        uploaded_desktop = st.file_uploader("📟檔案讀取：", type="xlsx")
+        if uploaded_desktop is not None:
+            st.write(f"檔案大小為{uploaded_desktop.size}")
+    #     # st.write(f"{pd.read_excel(uploaded_file).shape[0]}")
